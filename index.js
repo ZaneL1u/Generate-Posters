41f2a3b4919bfa2f609eb359f0d2a5bd3be22dd7
zhenze <ezreeeal@gmail.com>
2021-12-08 14:59:57 +0800

    fix: 修改为serverless部署

--- index.js
+++ index.js
@@ -1,40 +1,14 @@
-// 云函数入口文件
-const cloud = require('wx-server-sdk')
-const puppeteer = require('puppeteer')
-const fs = require('fs')
-cloud.init()
+const puppeteer = require("puppeteer");
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const port = 9000;
 
 String.prototype.interpolate = function (params) {
   const names = Object.keys(params);
   const vals = Object.values(params);
   return new Function(...names, `return \`${this}\`;`)(...vals);
-}
-
-// 云函数入口函数
-exports.main = async (event) => {
-  // 无头模式
-  const browser = await puppeteer.launch({args: ['--no-sandbox']})
-  const page = await browser.newPage()
-  try {
-    await page.setViewport({
-      width: event.width,
-      height: event.height
-    })
-    await page.setContent(getContent(event.template, event.fillData))
-    const image = await page.screenshot({
-      type: 'jpeg',
-      quality: 100,
-      fullPage: true
-    })
-    await browser.close()
-    return image
-  } catch (err) {
-    return {
-      code: 201,
-      message: err,
-    }
-  }
-}
+};
 
 /**
  * 根据模板获取html内容
@@ -42,6 +16,45 @@ exports.main = async (event) => {
  * @param {object} fillData 填充的数据
  */
 const getContent = (template, fillData) => {
-  let templateHTML = fs.readFileSync(`template/${template}.html`, 'utf8')
+  let templateHTML = fs.readFileSync(`template/${template}.html`, "utf8");
   return templateHTML.interpolate(fillData);
-}
+};
+
+const renderHTML = ({ width, height, template, ...fillData }) => {
+  return new Promise(async (resolve, reject) => {
+    const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
+    const page = await browser.newPage();
+    try {
+      await page.setViewport({ width: +width, height: +height });
+      await page.setContent(getContent(template, fillData));
+      const image = await page.screenshot({
+        type: "jpeg",
+        quality: 100,
+        fullPage: true,
+      });
+      await browser.close();
+      resolve(image);
+    } catch (err) {
+      console.log(err, err.message);
+      reject({
+        code: 202,
+        message: err,
+      });
+    }
+  });
+};
+
+app.get("/", async (req, res) => {
+  renderHTML(req.query)
+    .then((e) => {
+      res.header({ "Content-type": "image/jpeg" });
+      res.send(e);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
